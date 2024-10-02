8c7b47adfb8a8b1e9fb24a4d7b9212b0cee54a38
hwthinker <hwthinker@gmail.com>
2024-10-02 10:57:31 +0700

    first commit

--- src/02-neopixel/main.cpp
+++ src/02-neopixel/main.cpp
@@ -1,7 +1,7 @@
 #include<Arduino.h>
 #include <Adafruit_NeoPixel.h>
 
-#define PIN        8 //  根据需要修改  38 16 8
+#define PIN        48 //  根据需要修改  38 16 8
 #define NUMPIXELS  1
 
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
@@ -11,7 +11,7 @@ void setup() {
 }
 
 void loop() {
-  pixels.setPixelColor(0, pixels.Color(0,255,0)); // 红色
+  pixels.setPixelColor(0, pixels.Color(0,0,255)); // 红色
   pixels.show();
   delay(1000);
 }
